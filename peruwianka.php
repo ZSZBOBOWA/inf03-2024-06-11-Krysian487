d265104b8abbafe589fbeb10618c9b8fab055d9d
Krystian Potoczek <krystian.potoczek@zsz.bobowa.pl>
2024-12-03 14:44:37 +0100

    assa

---
+++ peruwianka.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+< lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Hodowla świnek morskich</title>
+    <link rel="stylesheet" href="styl.css">
+</head>
+<body>
+    <section class="baner">
+        <h1>Hodowla swinek morskich</h1>
+    </section>
+    <div class="lewy"></div>
+    <section class="lewyMenu">
+        <a href="peruwianka.php">Rasa Peruwianka</a>
+        <a href="american.php">Rasa American</a>
+        <a href="crested.php">Rasa Crested</a>
+    </section>
+    <section class="lewyGlowny">
+                <img src="peruwianka.jpg" alt="Swinka morska rasy peruwnaika">
+                <hr>
+        </section>
+        </body>
+        </div>
+            <section class="prawy">
+                <h3>Poznaj wszystkie rasy świnek morskich</h3>
+                <ol>
+                    2
+                </ol>
+                <hr>
+                <h2>Swinki w tym mocie</h2>
+                3
+            </section>
+            <footer><p>Strone wykonał:000000000</p></footer>
+        
+        </body>
+        </html>
